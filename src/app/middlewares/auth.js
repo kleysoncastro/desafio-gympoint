4ea90f7a0b557538df4aef4420ac5b977b72cce2
kleyson castro <kleysoncastro99@gmail.com>
2019-10-30 10:32:00 -0300

    fix: middleware, quando token é undegined

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -5,8 +5,14 @@ import authContig from '../../config/authConfig';
 export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
+  if (req.headers.authorization === undefined) {
+    return res.status(401).json({ erro: 'Token não informando' });
+  }
+  if (req.headers.authorization === null) {
+    return res.status(401).json({ erro: 'Token não aprovado' });
+  }
   if (!authContig) {
-    return res.status(401).json({ erro: 'Token nao aprovaado' });
+    return res.status(401).json({ erro: 'Token nao aprovado' });
   }
 
   const [, token] = authHeader.split(' ');
